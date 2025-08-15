ba24dcdb417c36400a453011a1aa33a955345302
Xumreen <xamreenajmal@gmail.com>
2025-08-15 16:35:05 +0500

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: './.env'  // Note the dot before 'env'
+})
+connectDB();
+
+
+
